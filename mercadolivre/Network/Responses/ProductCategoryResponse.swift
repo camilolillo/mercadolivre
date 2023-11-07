c6e1a2c44eaf22495a914d9bda3f5fc137be3d1e
Camilo <camilolillo>
2023-11-07 14:05:22 -0300

    Created Product Root Category model. Commit before pod init

---
+++ mercadolivre/Network/Responses/ProductCategoryResponse.swift
@@ -0,0 +1,12 @@
+//
+//  ProductCategoryResponse.swift
+//  mercadolivre
+//
+//  Created by Camilo Lillo on 07-11-23.
+//
+
+import Foundation
+
+struct ProductCategoryResponse: Codable {
+    let productRootCategories: [ProductRootCategory]
+}
