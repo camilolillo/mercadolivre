559c0ca47d9bfc74b738f83d39afcfcaab7b6b0c
Camilo <camilolillo>
2023-11-09 02:11:33 -0300

    Search view done

--- mercadolivre/Common/Extensions/String+Constants.swift
+++ mercadolivre/Common/Extensions/String+Constants.swift
@@ -11,6 +11,7 @@ extension String {
     static let defaultAlertMessage = "Hubo un problema con la solicitud. Puedes intentar denuevo."
     static let defaultAcceptActionTitle = "Aceptar"
     static let cancel = "Cancelar"
+    static let searchPlaceholder = "¿Que es lo que buscas?"
     
     //MARK: - Responses custom messages
     static let noRootCategoriesMessage = "En este momento no hay categorías para mostrar."
--- mercadolivre/Modules/SearchModule/SearchViewController.swift
+++ mercadolivre/Modules/SearchModule/SearchViewController.swift
@@ -8,6 +8,20 @@ import UIKit
 // MARK: - SearchViewController
 final class SearchViewController: BaseViewController {
     var presenter: SearchPresenterProtocol?
+    
+    private lazy var searchField: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.systemGray.withAlphaComponent(0.6).cgColor
+        field.layer.cornerRadius = 12
+        field.placeholder = .searchPlaceholder
+        let padding = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: field.frame.height))
+        field.leftView = padding
+        field.leftViewMode = .always
+        return field
+    }()
+    
 }
 
 // MARK: - Lifecycle
@@ -22,6 +36,15 @@ extension SearchViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         presenter?.onViewWillAppear()
+        
+        view.addSubview(searchField)
+        
+        NSLayoutConstraint.activate([
+            searchField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            searchField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12),
+            searchField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            searchField.heightAnchor.constraint(equalToConstant: 38)
+        ])
     }
 }
 
