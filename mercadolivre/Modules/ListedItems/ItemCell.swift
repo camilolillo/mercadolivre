66c36712cc3ef16776a998a9907b568806f75110
Camilo <camilolillo>
2023-11-09 00:57:34 -0300

    thumbnails working fine on item list

--- mercadolivre/Modules/ListedItems/ItemCell.swift
+++ mercadolivre/Modules/ListedItems/ItemCell.swift
@@ -15,6 +15,7 @@ final class ItemCell: DataSourceableCell {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 10)
+        label.numberOfLines = 0
         return label
     }()
     
@@ -24,6 +25,7 @@ final class ItemCell: DataSourceableCell {
         view.layer.cornerRadius = view.frame.size.width / 2
         view.clipsToBounds = true
         view.contentMode = .scaleAspectFill
+        view.image = UIImage(named: "main_logo")
         return view
     }()
         
@@ -31,6 +33,9 @@ final class ItemCell: DataSourceableCell {
         didSet {
             guard let dataSource else { return }
             title.text = dataSource.name
+            if let url = URL(string: dataSource.imageUrlString) {
+                imageView.kf.setImage(with: url)
+            }
         }
     }
     
@@ -38,9 +43,11 @@ final class ItemCell: DataSourceableCell {
         contentView.backgroundColor = .white
         contentView.layer.cornerRadius = 12
         contentView.addSubview(title)
+        contentView.addSubview(imageView)
         NSLayoutConstraint.activate([
             title.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
             title.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            title.trailingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: -8),
             imageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.8),
             imageView.widthAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.8),
             imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
